172ca57076c768f95dc9e140548d03a91d0ff2ee
JaschaThiel <jascha.thiel@gmail.com>
2018-05-16 15:16:15 +0200

    linked capture points with database

--- app/src/main/java/com/mamn01/pi/kingofcampus/CapturePoint.java
+++ app/src/main/java/com/mamn01/pi/kingofcampus/CapturePoint.java
@@ -21,13 +21,55 @@ public class CapturePoint {
     private Circle area;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public CapturePoint(String name, GoogleMap map, LatLng point){
+    public CapturePoint(String name, GoogleMap map, LatLng point, Long currentHolderLong){
         this.name = name;
+        int currentColor = Color.BLACK;
+        int currentHolder = Math.toIntExact(currentHolderLong);
+        switch (currentHolder){
+            case 1:
+                // Data
+                currentColor = Color.MAGENTA;
+                break;
+            case 2:
+                // Maskin
+                currentColor = Color.RED;
+                break;
+            case 3:
+                // K
+                currentColor = Color.YELLOW;
+                break;
+            case 4:
+                // F
+                currentColor = Color.BLACK;
+                break;
+            case 5:
+                // W
+                currentColor = Color.CYAN;
+                break;
+            case 6:
+                // E
+                currentColor = Color.WHITE;
+                break;
+            case 7:
+                // V
+                currentColor = Color.BLUE;
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            default:
+                currentColor = Color.BLACK;
+                break;
+
+        }
         area = map.addCircle(new CircleOptions()
                 .center(point)
                 .radius(20)
                 .strokeColor(Color.TRANSPARENT)
-                .fillColor(Color.rgb(0,0,255)));
+                .fillColor(currentColor));
         area.setClickable(true);
     }
 
--- app/src/main/java/com/mamn01/pi/kingofcampus/GameSettings.java
+++ app/src/main/java/com/mamn01/pi/kingofcampus/GameSettings.java
@@ -2,11 +2,20 @@ package com.mamn01.pi.kingofcampus;
 
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -14,15 +23,73 @@ import java.util.List;
  */
 
 public class GameSettings {
+    private static final String TAG = "GameSettings";
+    private static StorageReference mStorageReference;
+    private static FirebaseDatabase fDatabase;
+    private static DatabaseReference dataRef;
 
     public static List<CapturePoint> capturePointList = new ArrayList<>();
+    private static List<LatLng> capturePointLatLngList = new ArrayList<>();
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public static void init(GoogleMap mMap){
-        capturePointList.add(new CapturePoint("Kårhuset",mMap, new LatLng(55.712386, 13.209087)));
-        capturePointList.add(new CapturePoint("IKDC",mMap, new LatLng(55.715135, 13.212273)));
-        capturePointList.add(new CapturePoint("LED",mMap, new LatLng(55.710929, 13.210208)));
-    }
+    public static void init(final GoogleMap mMap){
 
+        capturePointLatLngList.add(new LatLng(55.713597, 13.213089)); // entrance
+        capturePointLatLngList.add(new LatLng(55.713431, 13.213732)); // bottom right
+        capturePointLatLngList.add(new LatLng(55.713663, 13.213504)); // middle
+        capturePointLatLngList.add(new LatLng(55.715135, 13.212273)); // IKDC
+
+        capturePointLatLngList.add(new LatLng(55.713846, 13.213207)); // top left
+
+        capturePointLatLngList.add(new LatLng(55.712386, 13.209087)); // Karhuset
+
+        capturePointLatLngList.add(new LatLng(55.710929, 13.210208)); // LED
+
+
+        mStorageReference = FirebaseStorage.getInstance().getReference();
+        fDatabase = FirebaseDatabase.getInstance();
+        dataRef = fDatabase.getReference("");
+
+        // Read from the database
+        dataRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                HashMap<String, Long> nodes;
+                nodes = (HashMap<String, Long>) dataSnapshot.getValue();
+                System.out.println("Size of the map: " + nodes.size());
+                System.out.println("received nodes from database");
+                addValuesToList(nodes, mMap);
+                Log.d(TAG, "Value is: " + nodes.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+//        capturePointList.add(new CapturePoint("Kårhuset",mMap, new LatLng(55.712386, 13.209087)));
+  //      capturePointList.add(new CapturePoint("IKDC",mMap, new LatLng(55.715135, 13.212273)));
+    //    capturePointList.add(new CapturePoint("LED",mMap, new LatLng(55.710929, 13.210208)));
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private static void addValuesToList(HashMap<String, Long> capturePoints, GoogleMap map) {
+        capturePointList = new ArrayList<>();
+        map.clear();
+        int i = 0;
+        System.out.println(capturePoints.keySet());
+        for (String key : capturePoints.keySet()) {
+            capturePointList.add(new CapturePoint(key, map, capturePointLatLngList.get(i), capturePoints.get(key)));
+            i ++;
+
+        }
+        System.out.println("The list contains " + capturePointList.size() + " Elements");
+
+    }
 
 }
